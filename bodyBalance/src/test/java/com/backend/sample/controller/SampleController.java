875cf47f731945a1fff17e2fd848148694d721cf
hgHwang <hgHwang@creativehill.co.kr>
2021-09-28 00:00:31 +0900

    최초 소스 커밋

---
+++ bodyBalance/src/test/java/com/backend/sample/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.backend.sample.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+	
+	public String sampleTest() {
+		return "";
+	}
+
+}
