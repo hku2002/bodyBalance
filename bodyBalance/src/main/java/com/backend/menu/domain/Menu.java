fe4f189cf30d91bdf8ab264c87bc13cf7e48f316
hgHwang <hgHwang@creativehill.co.kr>
2021-09-29 18:46:45 +0900

    Menu.java 소스 추가 (컬럼, 매핑 등 정의)

--- bodyBalance/src/main/java/com/backend/menu/domain/Menu.java
+++ bodyBalance/src/main/java/com/backend/menu/domain/Menu.java
@@ -1,5 +1,50 @@
 package com.backend.menu.domain;
 
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_MENU")
 public class Menu {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IDX")
+	private int idx;
+	
+	@Column(name = "MEAL_TYPE_CODE")
+	private String mealTypeCode;
+	
+	@Column(name = "DATE")
+	private Date date; 
+	
+	@Column(name = "MENU_TITLE")
+	private String menuTitle;
+	
+	@Lob
+	@Column(name = "MENU_CONTENT")
+	private String menuContent;
+	
+	@CreatedDate
+	@Column(name = "FIRST_REGISTER_TIME", updatable = false)
+	private LocalDateTime firstRegisterTime;
+	
+	@Column(name = "LAST_UPDATE_TIME", updatable = true)
+	private LocalDateTime lastUpdateTime;
 }
