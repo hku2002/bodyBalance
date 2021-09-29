90dddddcaae6bf376a78eb2c309c242b1ac7bbaa
hgHwang <hgHwang@creativehill.co.kr>
2021-09-29 17:54:03 +0900

    spring boot 동작 자바 파일 위치 변경

--- bodyBalance/src/main/java/com/backend/common/start/BodyBalanceApplication.java
+++ bodyBalance/src/main/java/com/backend/common/start/BodyBalanceApplication.java
@@ -1,4 +1,4 @@
-package com.backend.main;
+package com.backend.common.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
