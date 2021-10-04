637ed3f9c606e77f3e0bfd7e862f987161dccb8b
hgHwang <hgHwang@creativehill.co.kr>
2021-10-04 16:59:30 +0900

    MenuService 최초 생성

--- bodyBalance/src/main/java/com/backend/menu/service/MenuServiceImpl.java
+++ bodyBalance/src/main/java/com/backend/menu/service/MenuServiceImpl.java
@@ -2,7 +2,34 @@ package com.backend.menu.service;
 
 import org.springframework.stereotype.Service;
 
+import com.backend.menu.domain.Menu;
+import com.backend.menu.dto.MenuInsertRequestDto;
+
 @Service
 public class MenuServiceImpl {
-
+	
+	public Menu getOne(int idx) {
+		Menu menu = new Menu();
+		
+		return menu;
+	}
+	
+	public Menu getList(int memberNumber) {
+		Menu menu = new Menu();
+		
+		return menu;
+	}
+	
+	public Menu addOne(MenuInsertRequestDto dto) {
+		Menu menu = new Menu();
+		
+		return menu;
+	}
+	
+	public Menu setOne(MenuInsertRequestDto dto) {
+		Menu menu = new Menu();
+		
+		return menu;
+	}
+	
 }
